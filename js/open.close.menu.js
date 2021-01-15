6d0e3eba4f17dc16eccd25078fa7c9fc47d15188
John <mijailhernandez@gmail.com>
2021-01-15 13:50:54 -0500

    Added open menu styles and functionality.

---
+++ js/open.close.menu.js
@@ -0,0 +1,23 @@
+const menuBtn = document.getElementById('menu-btn');
+const logo = document.querySelector('.navbar__logo');
+const menuIcon = document.querySelector('.navbar__menu-icon');
+// const nav = document.getElementById('nav');
+const nav = document.getElementsByTagName('nav')[0];
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.setAttribute('src', 'images/icon-close.svg');
+        logo.classList.add('js-fixed-logo');
+        menuIcon.classList.add('js-fixed-menu');
+        nav.classList.add('js-navbar__nav');
+        menuOpen = true;
+    } else {
+        menuBtn.setAttribute('src', 'images/icon-hamburger.svg');
+        logo.classList.remove('js-fixed-logo');
+        menuIcon.classList.remove('js-fixed-menu');
+        nav.classList.remove('js-navbar__nav');
+        menuOpen = false;
+    }
+});
