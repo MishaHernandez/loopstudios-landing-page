9056d94ece12c14d65af601f76c38f067a87e37d
John <mijailhernandez@gmail.com>
2021-01-15 23:18:10 -0500

    Added animation on .interactive section

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,31 @@
 // Los elementos html que serán animados tendrán la clase 'animated'
-let animated = document.querySelectorAll('.move-up');
+
+let animated = document.querySelectorAll('.move-up-1, .move-up-2, .move-left');
 
 function showScroll() {
     let scrollTop = document.documentElement.scrollTop;
+
     for (let i = 0; i < animated.length; i++) {
         let scrollAnimated = animated[i].offsetTop;
-        if (scrollAnimated - 400 < scrollTop) {
-            animated[i].classList.add('js-remove-opacity');
-            animated[i].classList.add('js-move-up');
+
+        if (animated[i].matches('.move-up-1')) {
+            if (scrollAnimated - 400 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-up-1');
+            }
+        } else if (animated[i].matches('.move-up-2')) {
+            if (scrollAnimated + 100 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-up-2');
+            }
+        } else if (animated[i].matches('.move-left')) {
+            if (scrollAnimated + 100 < scrollTop) {
+                animated[i].classList.add('js-remove-opacity');
+                animated[i].classList.add('js-move-left');
+            }
         }
+
     }
 }
+
 window.addEventListener('scroll', showScroll);
